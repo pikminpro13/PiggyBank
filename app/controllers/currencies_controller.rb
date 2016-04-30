9878affaf54f533f785e92aa43ace6409433b76d
Eoin Broadfoot <tn_tiger99@yahoo.com>
2016-04-30 12:04:49 -0500

    fixed new form

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,4 +2,9 @@ class CurrenciesController < ApplicationController
 	def index
 		@currencies=Currency.all
 	end
+	def new
+		@currency= Currency.new
+	end
+	def create
+	end
 end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,6 @@
 class Currency < ActiveRecord::Base
   attr_accessible :country, :denomination, :name, :currency_type, :year
+  def test_method(var)
+    puts "hello #{var}"
+  end
 end
